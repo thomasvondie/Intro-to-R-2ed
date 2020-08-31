a96f18335368c489d6e4444db3f329514d6360fc
Thomas VonDerVellen <thv222@lehigh.edu>
2020-08-31 19:12:25 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,8 @@
 # R script to accompany Intro to R
-# Copy for Thomas VonDerVellen
-######################################################
-
-# You will see that you can put non-executable "comment" lines
-# in your script by putting "#" at the beginning of the lines.
 # 
-# In fact, you can put comments anywhere on a line as long as 
-# you put "#" before them.
+# Written by Troy A. Adair, Jr., Ph.D.
+
+# Copy for Thomas VonDerVellen
+# 
+# Customized by Thomas VonDerVellen
+##########################################################################
